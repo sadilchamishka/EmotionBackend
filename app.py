53a89ae1f2575873997e6c5bbb574e2aa55aa0fb
sadilchamishka <sandilchamishka@gmail.com>
2020-06-25 01:32:36 +0530

    upload arff file

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask,send_file,request,jsonify
 from flask_cors import CORS
 import pandas as pd
-import json
-import base64
+from scipy.io import arff
 
 # Create Flask app and enable CORS
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/emotion")
+@app.route("/emotion",methods = ['POST'])
 def predictEmotion():
+	f = request.files['file']
+	data_train = arff.loadarff(f)
     return "Success"
 
 if __name__ == "__main__":
